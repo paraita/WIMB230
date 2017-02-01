4719cc3312c962d0093c37bcae90163a9971f65c
Paraita Wohler <milkyboi@gmail.com>
2017-02-01 19:54:02 +0100

    Use pull-to-refresh to fetch bus passages

--- WIMB230/WIMB230/ViewController.swift
+++ WIMB230/WIMB230/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Alamofire
 import AlamofireObjectMapper
+import PullToRefresh
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -16,11 +17,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     
     var busPassages = [BusPassage]()
+    let refresher = PullToRefresh()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        fetchBusPassages()
+        self.tableView.addPullToRefresh(PullToRefresh()) {
+            self.fetchBusPassages()
+        }
+    }
+    
+    deinit {
+        self.tableView.removePullToRefresh(self.tableView.topPullToRefresh!)
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,9 +48,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     print(busPassage.is_real_time!)
                 }
                 print("]")
-                self.busPassages.removeAll()
-                self.mockData()
+                if (self.busPassages.count == 0) {
+                    self.mockData()
+                }
                 self.tableView.reloadData()
+                self.tableView.endRefreshing(at: .top)
             }
         }
     }
@@ -50,13 +60,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func mockData() {
         self.busPassages.removeAll()
         let b1 = BusPassage(bus_time: "2017-01-29 17:15:01",
-                            dest: "Cathédrale Vielle-Ville",
+                            dest: "Mocked Cathédrale Vielle-Ville",
                             is_real_time: true)
         let b2 = BusPassage(bus_time: "2017-01-29 17:22:02",
-                            dest: "Cathédrale Vielle-Ville",
+                            dest: "Mocked Cathédrale Vielle-Ville",
                             is_real_time: false)
         let b3 = BusPassage(bus_time: "2017-01-29 17:32:03",
-                            dest: "Cathédrale Vielle-Ville",
+                            dest: "Mocked Cathédrale Vielle-Ville",
                             is_real_time: false)
         self.busPassages.append(b1)
         self.busPassages.append(b2)
@@ -89,7 +99,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.detailTextLabel!.text = busPassage.dest
     }
     
+}
+
+private extension ViewController {
     
-    
+    func setupPullToRefresh() {
+        tableView.addPullToRefresh(PullToRefresh()) { [weak self] in
+            let delayTime = DispatchTime.now() + Double(Int64(2 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+            DispatchQueue.main.asyncAfter(deadline: delayTime) {
+                self?.tableView.endRefreshing(at: .top)
+            }
+        }
+    }
 }
 
