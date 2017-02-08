4f01821ae9ed27ec531a011714b3c5939f2bb77c
Paraita Wohler <milkyboi@gmail.com>
2017-02-08 22:30:06 +0100

    Add playground to test functions

---
+++ WIMB230/WIMB230/TestStuff.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+import UIKit
+
+
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+let dateNow = Date()
+
+let date1 = dateFormatter.date(from: "2017-02-08 21:33:42")
+
+date1!.timeIntervalSince(dateNow) / 60
