308ebb46bf31642d9d04ce343f8df268c0a77370
Paraita Wohler <milkyboi@gmail.com>
2017-02-15 14:22:14 +0100

    Add the fetching function to the widget

--- WIMB230/WIMB230/WIMB230Client.swift
+++ WIMB230/WIMB230/WIMB230Client.swift
@@ -7,6 +7,7 @@
 //
 import Foundation
 import Alamofire
+import AlamofireObjectMapper
 import UIKit
 
 class WIMB230Client {
--- WIMB230/WIMB230Widget/TodayViewController.swift
+++ WIMB230/WIMB230Widget/TodayViewController.swift
@@ -11,9 +11,68 @@ import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+    @IBOutlet var mainLabel: UILabel!
+    let client = WIMB230Client()
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
+        self.extensionContext?.widgetLargestAvailableDisplayMode = NCWidgetDisplayMode.expanded
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(refreshLabel),
+                                               name: NSNotification.Name(rawValue: "fetchedData"),
+                                               object: nil)
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    }
+    
+    func refreshLabel() {
+        var strResult = ""
+        let dateNow = Date()
+        
+        for bp in self.client.busPassages {
+            let dateBusRaw = bp.bus_time!
+            
+            let regex = try! NSRegularExpression(pattern: "\\..*$", options: NSRegularExpression.Options.caseInsensitive)
+            let range = NSMakeRange(0, dateBusRaw.characters.count)
+            let cleanDateBus = regex.stringByReplacingMatches(in: dateBusRaw, options: [], range: range, withTemplate: "")
+                .replacingOccurrences(of: "T", with: " ")
+            let dateBus = self.dateFormatter.date(from: cleanDateBus)
+            let deltaTime = dateBus?.timeIntervalSince(dateNow)
+            let busTimeInt = lround(deltaTime! / 60)
+            var busTimeStr = "In \(busTimeInt) "
+            if (busTimeInt > 1) {
+                busTimeStr += "minutes !"
+            }
+            else {
+                busTimeStr += "minute !!!"
+            }
+            if (!bp.is_real_time!) {
+                busTimeStr += " *"
+            }
+            strResult += busTimeStr
+            if (isAPromenadeBus(busPassage: bp)) {
+                strResult += " (Promenade)"
+            }
+            else {
+                strResult += " (Nice Nord)"
+            }
+            strResult += "\n"
+        }
+        self.mainLabel.text = strResult
+    }
+    
+    func isAPromenadeBus(busPassage: BusPassage) -> Bool {
+        return busPassage.dest == "Cathédrale-Vieille Ville"
+    }
+    
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize) {
+        if (activeDisplayMode == NCWidgetDisplayMode.compact) {
+            self.preferredContentSize = maxSize
+        }
+        else {
+            self.preferredContentSize = CGSize(width: 0, height: 200)
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,6 +86,8 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
+        
+        self.client.fetchBusPassages(stopId: 1939)
         
         completionHandler(NCUpdateResult.newData)
     }
